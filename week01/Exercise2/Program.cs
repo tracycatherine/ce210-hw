a2f565a50eb31f34ee074f6f397ce0f72c4e5b79
tracycatherine <tracynalubwama5@gmail.com>
2025-03-11 12:40:12 +0300

    nnnbb

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -2,8 +2,76 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args )
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        string sign = "";
+
+        // letter grading
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // "-" and "+" on all letters 
+        if (letter != "F" && letter != "A")
+        {
+            int lastDigit = grade % 10;
+
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        // "-" and "+" A and F letters
+        if (letter == "A" && grade < 97)
+        {
+            sign = "-";
+        }
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        // actaul grades
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        // Performance
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying! You'll get it next time.");
+        }
     }
+
+
 }
